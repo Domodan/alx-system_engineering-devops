755b51a1d907180485f2a98a8a0653b1e1bb02fa
domodan <ongomdaniel9@gmail.com>
2023-10-23 01:04:59 -0700

    Export the API data retrieved to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+    Exports data in the JSON format
+"""
+
+
+from requests import get
+from sys import argv
+from json import dump
+
+
+if __name__ == "__main__":
+
+    userId = argv[1]
+    user = get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+
+    todos = get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+
+    todoUser = {}
+    taskList = []
+
+    for task in todos:
+        if task.get('userId') == int(userId):
+            taskDict = {"task": task.get('title'),
+                        "completed": task.get('completed'),
+                        "username": user.json().get('username')}
+            taskList.append(taskDict)
+    todoUser[userId] = taskList
+
+    filename = userId + '.json'
+    with open(filename, mode='w') as f:
+        dump(todoUser, f)
